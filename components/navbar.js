006ae397086b8f18285d5c9cf76df89e4eed14db
Alan Guerra <contato@esalanguerra.com.br>
2025-03-13 12:43:05 -0300

    alterando navbar

--- components/navbar.js
+++ components/navbar.js
@@ -80,16 +80,9 @@ const Navbar = props => {
           <LinkItem href="/works" path={path}>
             Works
           </LinkItem>
-          <LinkItem href="/wallpapers" path={path}>
-            Wallpapers
-          </LinkItem>
-          <LinkItem href="/posts" path={path}>
-            Posts
-          </LinkItem>
-          <LinkItem href="https://uses.craftz.dog/">Uses</LinkItem>
           <LinkItem
             target="_blank"
-            href="https://github.com/craftzdog/craftzdog-homepage"
+            href="https://github.com/esalanguerraoficial/esalanguerra.com.br"
             path={path}
             display="inline-flex"
             alignItems="center"
@@ -119,18 +112,9 @@ const Navbar = props => {
                 <MenuItem as={MenuLink} href="/works">
                   Works
                 </MenuItem>
-                <MenuItem as={MenuLink} href="/wallpapers">
-                  Wallpapers
-                </MenuItem>
-                <MenuItem as={MenuLink} href="/posts">
-                  Posts
-                </MenuItem>
-                <MenuItem as={MenuLink} href="https://uses.craftz.dog/">
-                  Uses
-                </MenuItem>
                 <MenuItem
                   as={Link}
-                  href="https://github.com/craftzdog/craftzdog-homepage"
+                  href="https://github.com/esalanguerraoficial/esalanguerra.com.br"
                 >
                   View Source
                 </MenuItem>
